dc63262fa2109a8c691c3babf41f0ba0799e624d
kishima <kishima7@gmail.com>
2025-07-14 01:29:30 +0900

    update tool script

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+TARGET_ELF_FILE=R2P2_PICO2-FLASH_MSC-.elf
+
+MYPC=1 SKIP_TEST=1 rake mrubyc:pico2:debug
+
+# Check if "flash" is in the arguments
+if [[ "$1" == "flash" ]]; then
+    openocd -f interface/cmsis-dap.cfg -f target/rp2350.cfg -c "adapter speed 2000" -c "program build_pico2/$TARGET_ELF_FILE verify reset exit"
+else
+    echo "Build successful. Use './build.sh flash' to write to device."
+fi
--- flash.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-TARGET_ELF_FILE=R2P2_PICO2-FLASH_MSC-.elf
-
-MYPC=1 SKIP_TEST=1 rake mrubyc:pico2:debug
-
-openocd -f interface/cmsis-dap.cfg -f target/rp2350.cfg -c "adapter speed 2000" -c "program build_pico2/$TARGET_ELF_FILE verify reset exit"
